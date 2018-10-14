a93fac5fc14d6b23726522b4f658c1861d3d48c3
1998htt <3133437521@qq.com>
2018-10-14 10:58:11 +0800

    login

--- login.php
+++ login.php
@@ -8,9 +8,21 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="server.php">
-        用户名：<input type="text" name="username"></br>
-    密码： <input type="password" name="pwd"></br>
+    <form action="server.php" method="post">
+        用户名：<input type="text" name="username"><br/>
+        密码： <input type="password" name="pwd"><br/>
+        <input type="radio" name="sex" value="f">女
+        <input type="radio" name="sex" value="m">男<br/>
+        <input type="checkbox" name="hobby[]" value="篮球">篮球
+        <input type="checkbox" name="hobby[]" value="足球">足球
+        <input type="checkbox" name="hobby[]" value="排球">排球
+        <input type="checkbox" name="hobby[]" value="手球">手球<br/>
+        <select name="school[]" id="" multiple="multiple">
+            <option value="黑大" >黑大</option>
+            <option value="林大">林大</option>
+            <option value="农大">农大</option>
+            <option value="理工">理工</option>
+        </select>
         <input type="submit" value="登录">
     </form>
 
--- server.php
+++ server.php
@@ -5,6 +5,11 @@
  * Date: 2018/10/14
  * Time: 9:26
  */
-$username = $_GET['username'];
-$pwd = $_GET['pwd'];
-echo "您的用户名为:".$username."您的密码为:".$pwd;
+$username = $_POST['username'];
+$pwd = $_POST['pwd'];
+$s = $_POST['sex'];
+$h = $_POST['hobby'];
+$m = $_POST['school'];
+var_dump($h);
+var_dump($m);
+echo "您的用户名为:".$username,"您的密码为:".$pwd,$s;
