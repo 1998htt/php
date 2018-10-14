7fb01b3b4220f7a61aca2294f9aabc70b80495b4
1998htt <3133437521@qq.com>
2018-10-14 11:27:52 +0800

    login

--- login.php
+++ login.php
@@ -1,3 +1,18 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $url = "server.php";
+        $username = $_POST['username'];
+        $pwd = $_POST['pwd'];
+        if($username == 'abc' && $pwd == '123'){
+            echo "登录成功";
+            header('Location:' . $url);
+
+        }
+        else
+            echo "用户名或密码错误";
+    }
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,23 +23,24 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="server.php" method="post">
+   <form action="login.php" method="post">
         用户名：<input type="text" name="username"><br/>
         密码： <input type="password" name="pwd"><br/>
-        <input type="radio" name="sex" value="f">女
-        <input type="radio" name="sex" value="m">男<br/>
-        <input type="checkbox" name="hobby[]" value="篮球">篮球
-        <input type="checkbox" name="hobby[]" value="足球">足球
-        <input type="checkbox" name="hobby[]" value="排球">排球
-        <input type="checkbox" name="hobby[]" value="手球">手球<br/>
-        <select name="school[]" id="" multiple="multiple">
-            <option value="黑大" >黑大</option>
-            <option value="林大">林大</option>
-            <option value="农大">农大</option>
-            <option value="理工">理工</option>
-        </select>
+<!--        <input type="radio" name="sex" value="f">女-->
+<!--        <input type="radio" name="sex" value="m">男<br/>-->
+<!--        <input type="checkbox" name="hobby[]" value="篮球">篮球-->
+<!--        <input type="checkbox" name="hobby[]" value="足球">足球-->
+<!--        <input type="checkbox" name="hobby[]" value="排球">排球-->
+<!--        <input type="checkbox" name="hobby[]" value="手球">手球<br/>-->
+<!--        <select name="school[]" id="" multiple="multiple">-->
+<!--            <option value="黑大" >黑大</option>-->
+<!--            <option value="林大">林大</option>-->
+<!--            <option value="农大">农大</option>-->
+<!--            <option value="理工">理工</option>-->
+<!--        </select>-->
         <input type="submit" value="登录">
     </form>
+
 
 </body>
 </html>
--- server.php
+++ server.php
@@ -5,11 +5,17 @@
  * Date: 2018/10/14
  * Time: 9:26
  */
-$username = $_POST['username'];
-$pwd = $_POST['pwd'];
-$s = $_POST['sex'];
-$h = $_POST['hobby'];
-$m = $_POST['school'];
-var_dump($h);
-var_dump($m);
-echo "您的用户名为:".$username,"您的密码为:".$pwd,$s;
+echo "登陆成功";
+//$username = $_POST['username'];
+//$pwd = $_POST['pwd'];
+//if($username == 'abc'&& $pwd =='123'){
+//    echo "登录成功";
+//}
+//else
+//    echo "登录失败";
+//$s = $_POST['sex'];
+//$h = $_POST['hobby'];
+//$m = $_POST['school'];
+//var_dump($h);
+//var_dump($m);
+//echo "您的用户名为:".$username,"您的密码为:".$pwd,$s;
