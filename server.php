a4620970dc1c510e2d9ea9e080abdd2fce7e2a5f
1998htt <3133437521@qq.com>
2018-10-14 09:31:25 +0800

    login

---
+++ server.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: de
+ * Date: 2018/10/14
+ * Time: 9:26
+ */
+$username = $_GET['username'];
+$pwd = $_GET['pwd'];
+echo "您的用户名为:".$username."您的密码为:".$pwd;
