d2b610527eedae98823b73033033af8eee9f9848
1998htt <3133437521@qq.com>
2018-10-26 17:56:12 +0800

    login

---
+++ dbms.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    创建表：<input type="text" id="creat">
+    <button id="btn1">创建表</button>
+    <script src="jquery-1.12.4.js"></script>
+    <script>
+        $(function(){
+            $('#btn1').on('click',function(){
+
+                var creat = $('#creat').val();
+                if(creat == "ceate table sc(sno char(9),cno char(9),grade float"){
+                    $myfile = fopen("sc.txt", "w")
+                }
+
+            })
+
+
+        })
+
+
+    </script>
+</body>
+</html>
